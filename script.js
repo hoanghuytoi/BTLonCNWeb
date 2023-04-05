a39a83a075673dcb41313e01e637704f021fddc7
hoanghuytoi <hoanghuytoi03@Gmail.com>
2023-04-05 23:16:04 +0700

    add login

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-<button onclick="index.href='product.html'">Chuyển đến sản phẩm</button>
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogIn');
+const iconClose = document.querySelector('.icon-close');
+
+
+registerLink.addEventListener('click',()=>{
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click',()=>{
+    wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
+});
+
+
 
 var navLinks = document.getElementById("navLinks");
 
@@ -8,3 +30,4 @@ function showMenu(){
 function hideMenu(){
     navLinks.style.right = "-200px";
 }
+
